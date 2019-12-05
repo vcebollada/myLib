17c945074ee95edb0d72f1af0735c11087e16427
Victor Cebollada <v.cebollada@samsung.com>
2019-12-05 15:39:56 +0000

    Test lib
    Signed-off-by: Victor Cebollada <v.cebollada@samsung.com>

---
+++ MyLib.cpp
@@ -0,0 +1,16 @@
+#include "MyLib.h"
+
+#include <iostream>
+#include <png.h>
+#include <zlib.h>
+
+namespace MyLib
+{
+
+void PrintInfo()
+{
+	std::cout << "   Compiled with libpng " << PNG_LIBPNG_VER_STRING << "; using libpng " << png_libpng_ver << "." << std::endl;
+	std::cout << "   Compiled with zlib " << ZLIB_VERSION << "; using zlib " << zlib_version << " ." << std::endl;
+}
+
+}
---
+++ MyLib.h
@@ -0,0 +1,9 @@
+#ifndef MY_LIB_H
+#define MY_LIB_H
+
+namespace MyLib
+{
+void PrintInfo();
+}
+
+#endif
